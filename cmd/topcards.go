aa9c220f6cb0ee468b4f470fba813a0eb394aa1c
Jan Fanslau <office@jan-fanslau.de>
2024-02-25 17:16:59 +0100

    cleanup

--- cmd/topcards.go
+++ cmd/topcards.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 
+	"github.com/falsanu/topcards/model"
 	"github.com/wcharczuk/go-chart"
 )
 
@@ -16,15 +17,9 @@ func main() {
 	flag.Parse()
 	fmt.Printf("Playing with %d Cards\n", numberOfCards)
 
-	// var staple []model.Card
-	// var XValues []float64
-	//var YValues []float64
+	var staple []model.Card
 
-	// type byValue []model.Card
 	YValues := make([]int, numberOfCards, numberOfCards)
-	// func (a byValue) Len() float64           { return len(a) }
-	// func (a byValue) Less(i, j float64) bool { return a[i].Value < a[j].Value }
-	// func (a byValue) Swap(i, j float64)      { a[i], a[j] = a[j], a[i] }
 
 	for i := 0; i < numberOfCards; i++ {
 
@@ -33,21 +28,15 @@ func main() {
 
 		YValues[int(sample)] = YValues[int(sample)] + 1
 		fmt.Println(sample, YValues[int(sample)])
-		// staple = append(staple, model.Card{Value: sample})
-		// XValues = append(XValues, +sample)
-		//XValues = append(XValues, float64(i))
+		staple = append(staple, model.Card{Value: sample})
 
 	}
 	var x, y []float64
-	// //slices.Sort(YValues)
-	// // gett the keys out of YValues
 	for key, value := range YValues {
 		x = append(x, float64(key))
 		y = append(y, float64(value))
 		fmt.Printf("%d,%d\n", key, value)
 	}
-
-	// sort.Sort(byValue(staple))
 
 	graph := chart.Chart{
 		Series: []chart.Series{
@@ -63,15 +52,6 @@ func main() {
 	graph.Render(chart.PNG, f)
 
 }
-func FuncVar(f32 []int) []float64 {
-	f64 := make([]float64, len(f32))
-	var f int
-	var i int
-	for i, f = range f32 {
-		f64[i] = float64(f)
-	}
-	return f64
-}
 
 func TruncatedNormal(mean, stdDev, low, high float64) float64 {
 	if low >= high {
@@ -83,6 +63,5 @@ func TruncatedNormal(mean, stdDev, low, high float64) float64 {
 		if low <= x && x < high {
 			return x
 		}
-		// fmt.Println("missed!", x)
 	}
 }
